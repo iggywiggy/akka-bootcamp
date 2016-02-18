450d5893d5dfa307db04d7e1baaf5bc511b4923a
iggywiggy <lynnsaymay.piper@outlook.com>
2016-02-18 14:54:00 +0000

    Unit One Lesson Two

--- src/Unit-1/DoThis/ConsoleReaderActor.cs
+++ src/Unit-1/DoThis/ConsoleReaderActor.cs
@@ -4,13 +4,15 @@ using Akka.Actor;
 namespace WinTail
 {
     /// <summary>
-    /// Actor responsible for reading FROM the console. 
+    /// Actor responsible for reading FROM the console.
     /// Also responsible for calling <see cref="ActorSystem.Shutdown"/>.
     /// </summary>
-    class ConsoleReaderActor : UntypedActor
+    internal class ConsoleReaderActor : UntypedActor
     {
         public const string ExitCommand = "exit";
-        private IActorRef _consoleWriterActor;
+        public const string StartCommand = "start";
+
+        private readonly IActorRef _consoleWriterActor;
 
         public ConsoleReaderActor(IActorRef consoleWriterActor)
         {
@@ -19,19 +21,50 @@ namespace WinTail
 
         protected override void OnReceive(object message)
         {
-            var read = Console.ReadLine();
-            if (!string.IsNullOrEmpty(read) && String.Equals(read, ExitCommand, StringComparison.OrdinalIgnoreCase))
-            {
-                // shut down the system (acquire handle to system via
-                // this actors context)
-                Context.System.Shutdown();
-                return;
-            }
+            if (message.Equals(StartCommand))
+                DoPrintInstructions();
+            else if(message is Messages.InputError)
+                _consoleWriterActor.Tell(message as Messages.InputError);
 
-            _consoleWriterActor.Tell(read);
-
-            Self.Tell("continue");
+            GetAndValidateInput();
         }
 
+        private void GetAndValidateInput()
+        {
+            var message = Console.ReadLine();
+
+            if(string.IsNullOrEmpty(message))
+                Self.Tell(new Messages.NullInputError("No input received."));
+            else if (string.Equals(message, ExitCommand, StringComparison.OrdinalIgnoreCase))
+                Context.System.Terminate();
+            else
+            {
+                var valid = IsValid(message);
+                if (valid)
+                {
+                    _consoleWriterActor.Tell(new Messages.InputSuccess("Thank you! Message was valid."));
+                    Self.Tell(new Messages.ContinueProccessing());
+                }
+                else
+                {
+                    Self.Tell(new Messages.ValidationError("Invalid: input had odd number of characters."));
+                }
+            }
+            
+        }
+
+        private static bool IsValid(string message)
+        {
+            return message.Length%2 == 0;
+        }
+
+     
+
+        private void DoPrintInstructions()
+        {
+            Console.WriteLine("Write whatever you want into the console!");
+            Console.WriteLine("Some entries will pass validation, and some won't...\n\n");
+            Console.WriteLine("Type 'exit' to quit this application at any time.\n");
+        }
     }
 }
--- src/Unit-1/DoThis/Program.cs
+++ src/Unit-1/DoThis/Program.cs
@@ -1,44 +1,28 @@
 ﻿using System;
-﻿using Akka.Actor;
-using Akka.Actor.Dsl;
+using Akka.Actor;
 
 namespace WinTail
 {
     #region Program
-    class Program
+
+    internal class Program
     {
         public static ActorSystem MyActorSystem;
 
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            MyActorSystem = ActorSystem.Create("MyActorSystem");
+            MyActorSystem = ActorSystem.Create(nameof(MyActorSystem));
             var consoleWriterActor = MyActorSystem.ActorOf(Props.Create(() => new ConsoleWriterActor()));
             var consoleReaderActor =
                 MyActorSystem.ActorOf(Props.Create(() => new ConsoleReaderActor(consoleWriterActor)));
 
-            PrintInstructions();
-
-            consoleReaderActor.Tell("start");
+            consoleReaderActor.Tell(ConsoleReaderActor.StartCommand);
 
             // blocks the main thread from exiting until the actor system is shut down
             MyActorSystem.AwaitTermination();
         }
 
-        private static void PrintInstructions()
-        {
-            Console.WriteLine("Write whatever you want into the console!");
-            Console.Write("Some lines will appear as");
-            Console.ForegroundColor = ConsoleColor.DarkRed;
-            Console.Write(" red ");
-            Console.ResetColor();
-            Console.Write(" and others will appear as");
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.Write(" green! ");
-            Console.ResetColor();
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine("Type 'exit' to quit this application at any time.\n");
-        }
     }
-    #endregion
+
+    #endregion Program
 }
